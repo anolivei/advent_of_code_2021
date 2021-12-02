62c9bfa2a6b8c25da02aac340bc4bb88d61903ad
anolivei <anolivei@student.42sp.org.br>
2021-12-02 14:50:36 -0300

    flake8

--- day01_Sonar_Sweep/day01.py
+++ day01_Sonar_Sweep/day01.py
@@ -1,27 +1,30 @@
 def open_input():
-    with open ("input.txt") as fd:
+    with open("input.txt") as fd:
         array = fd.read().splitlines()
         fd.close()
     array = list(map(int, array))
     return array
+
 
 def larger_measurements(array):
     lenght = len(array)
     increased = 0
     for i in range(0, lenght - 1):
         if array[i] < array[i + 1]:
-            increased+=1
-    print("part one:",increased)
+            increased += 1
+    print("part one:", increased)
+
 
 def larger_measurements_three(array):
     lenght = len(array)
     increased = 0
     for i in range(0, lenght - 3):
-        sum1 = array[i] + array [i + 1] + array[i + 2]
-        sum2 = array [i + 1] + array[i + 2] + array[i + 3]
+        sum1 = array[i] + array[i + 1] + array[i + 2]
+        sum2 = array[i + 1] + array[i + 2] + array[i + 3]
         if sum1 < sum2:
-            increased+=1
-    print("part two:",increased)
+            increased += 1
+    print("part two:", increased)
+
 
 if (__name__ == "__main__"):
     array = open_input()
