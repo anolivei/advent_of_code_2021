1ebdf56aca00cf5ee0ff807fd7143c6b7997a2f4
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2021-12-04 16:42:10 -0300

    day04 part 2

--- day04_Giant_Squid/day04.py
+++ day04_Giant_Squid/day04.py
@@ -1,15 +1,41 @@
 import numpy as np
 
 
+def part_one(num):
+    rows = get_rows()
+    cols = get_cols(rows)
+    for n in range(0, len(num)):
+        for i in range(0, len(rows)):
+            if sum(rows[i]) == -5 or sum(cols[i]) == -5:
+                break
+            for j in range(0, 5):
+                if sum(rows[i]) == -5 or sum(cols[i]) == -5:
+                    break
+                if rows[i][j] == num[n]:
+                    rows[i][j] = -1
+                if cols[i][j] == num[n]:
+                    cols[i][j] = -1
+        if sum(rows[i]) == -5 or sum(cols[i]) == -5:
+            break
+    first_line = i - (i % 5)
+    last_line = first_line + 5
+    result = 0
+    for i in range(first_line, last_line):
+        for j in range(0, 5):
+            if rows[i][j] > 0:
+                result += rows[i][j]
+    print("part one:", num[n - 1] * result)
+
+
 def get_chosen_numbers():
-    with open("input.txt") as fd:
+    with open("input_test.txt") as fd:
         array = fd.read().splitlines()
     num = [int(s) for s in array[0].split(',')]
     return(num)
 
 
 def get_rows():
-    rows = np.loadtxt("input.txt", skiprows=1, dtype=int)
+    rows = np.loadtxt("input_test.txt", skiprows=1, dtype=int)
     return rows
 
 
@@ -35,37 +61,36 @@ def split(arr, size):
     return arrs
 
 
-def part_one(num, rows, cols):
-    for n in range(0, len(num)):
-        for i in range(0, len(rows)):
-            if sum(rows[i]) == -5 or sum(cols[i]) == -5:
-                break
-            for j in range(0, 5):
-                if sum(rows[i]) == -5 or sum(cols[i]) == -5:
+def part_two(num):
+    with open("input_test.txt") as fd:
+        array = fd.read().splitlines()
+    array = array[1:]
+    a_matrix = []
+    winners = []
+    n_boards = 0
+    i = 0
+    while i < len(array):
+        a_matrix.append(np.loadtxt(array[i:], dtype=int, max_rows=5))
+        i += 6
+        n_boards += 1
+    for n in num:
+        for i in range(0, n_boards):
+            a_matrix[i] = np.where(a_matrix[i] == n, -1, a_matrix[i])
+            row = np.where(a_matrix[i].sum(axis=0) == -5)
+            col = np.where(a_matrix[i].sum(axis=1) == -5)
+            if row[0].size > 0 or col[0].size > 0:
+                if i not in winners:
+                    winners.append(i)
+                if len(winners) == n_boards:
                     break
-                if rows[i][j] == num[n]:
-                    rows[i][j] = -1
-                if cols[i][j] == num[n]:
-                    cols[i][j] = -1
-        if sum(rows[i]) == -5 or sum(cols[i]) == -5:
+        if len(winners) == n_boards:
             break
-    first_line = i - (i % 5)
-    last_line = first_line + 5
-    result = 0
-    for i in range(first_line, last_line):
-        for j in range(0, 5):
-            if rows[i][j] > 0:
-                result += rows[i][j]
-    print("part one:", num[n - 1] * result)
-
-
-def part_two(num, rows, cols):
-    print("part two:")
+    a_matrix[i] = np.where(a_matrix[i] == -1, 0, a_matrix[i])
+    row = sum(a_matrix[i].sum(axis=0))
+    print("part two:", row * n)
 
 
 if (__name__ == "__main__"):
     num = get_chosen_numbers()
-    rows = get_rows()
-    cols = get_cols(rows)
-    part_one(num, rows, cols)
-    part_two(num, rows, cols)
+    part_one(num)
+    part_two(num)
