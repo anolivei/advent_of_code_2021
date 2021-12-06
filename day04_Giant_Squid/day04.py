e3403ea3fc23936cf3f9e687e8cd17e2d2151df2
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2021-12-05 21:45:12 -0300

    day04 rev

--- day04_Giant_Squid/day04.py
+++ day04_Giant_Squid/day04.py
@@ -1,32 +1,6 @@
 import numpy as np
 
 
-def part_one(num):
-    rows = get_rows()
-    cols = get_cols(rows)
-    for n in range(0, len(num)):
-        for i in range(0, len(rows)):
-            if sum(rows[i]) == -5 or sum(cols[i]) == -5:
-                break
-            for j in range(0, 5):
-                if sum(rows[i]) == -5 or sum(cols[i]) == -5:
-                    break
-                if rows[i][j] == num[n]:
-                    rows[i][j] = -1
-                if cols[i][j] == num[n]:
-                    cols[i][j] = -1
-        if sum(rows[i]) == -5 or sum(cols[i]) == -5:
-            break
-    first_line = i - (i % 5)
-    last_line = first_line + 5
-    result = 0
-    for i in range(first_line, last_line):
-        for j in range(0, 5):
-            if rows[i][j] > 0:
-                result += rows[i][j]
-    print("part one:", num[n - 1] * result)
-
-
 def get_chosen_numbers():
     with open("input.txt") as fd:
         array = fd.read().splitlines()
@@ -34,34 +8,7 @@ def get_chosen_numbers():
     return(num)
 
 
-def get_rows():
-    rows = np.loadtxt("input.txt", skiprows=1, dtype=int)
-    return rows
-
-
-def get_cols(rows):
-    col = []
-    m = 0
-    while m < len(rows):
-        for j in range(0, 5):
-            for i in range(m, m + 5):
-                col.append(rows[i][j])
-        m += 5
-    col = np.array(split(col, 5))
-    return col
-
-
-def split(arr, size):
-    arrs = []
-    while len(arr) > size:
-        pice = arr[:size]
-        arrs.append(pice)
-        arr = arr[size:]
-    arrs.append(arr)
-    return arrs
-
-
-def part_two(num):
+def solution(num, part):
     with open("input.txt") as fd:
         array = fd.read().splitlines()
     array = array[1:]
@@ -81,13 +28,29 @@ def part_two(num):
             if row[0].size > 0 or col[0].size > 0:
                 if i not in winners:
                     winners.append(i)
+            if part == "one":
+                if len(winners) == 1:
+                    break
+            elif part == "two":
                 if len(winners) == n_boards:
                     break
-        if len(winners) == n_boards:
-            break
+        if part == "one":
+            if len(winners) == 1:
+                break
+        elif part == "two":
+            if len(winners) == n_boards:
+                break
     a_matrix[i] = np.where(a_matrix[i] == -1, 0, a_matrix[i])
     row = sum(a_matrix[i].sum(axis=0))
-    print("part two:", row * n)
+    return (row * n)
+
+def part_one(num):
+    ret = solution(num, "one")
+    print("part one:", ret)
+
+def part_two(num):
+    ret = solution(num, "two")
+    print("part two:", ret)
 
 
 if (__name__ == "__main__"):
