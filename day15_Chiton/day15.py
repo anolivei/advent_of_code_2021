897d42151505844a3f68df37f8dbb84164d9eded
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2021-12-19 02:08:36 -0300

    day15

--- day15_Chiton/day15.py
+++ day15_Chiton/day15.py
@@ -11,27 +11,22 @@ def open_input():
     return matrix
 
 
-def check_borders(i, j, i_max, j_max):
-    if i < 0 or i > i_max or j < 0 or j > j_max:
-        return False
-    return True
-
-
 def neighborhood(i_max, j_max, i, j):
     neighbors = [
-        (i + 1, j),
-        (i - 1, j),
-        (i, j + 1),
-        (i, j - 1),
+                    (i + 1, j),
+                    (i - 1, j),
+                    (i, j + 1),
+                    (i, j - 1),
     ]
     n = []
     for neighbor in neighbors:
-        if check_borders(neighbor[0], neighbor[1], i_max, j_max):
-            n.append(neighbor)
+        if i < 0 or i > i_max or j < 0 or j > j_max:
+            continue
+        n.append(neighbor)
     return n
 
 
-def matrix_5x5(matrix, point):
+def value(matrix, point):
     i_len = len(matrix)
     j_len = len(matrix[0])
     i = point[0] % i_len
@@ -42,37 +37,33 @@ def matrix_5x5(matrix, point):
     return ret
 
 
-def solution(part):
+def solution(n_tiles):
     matrix = open_input()
-    multiplicator = 1 if part == "one" else 5
-    i_max = multiplicator * len(matrix) - 1
-    j_max = multiplicator * len(matrix[0]) - 1
+    i_max = n_tiles * len(matrix) - 1
+    j_max = n_tiles * len(matrix[0]) - 1
     delta_s = defaultdict(lambda: 99999999)
     delta_s[(0, 0)] = 0
-    past_points = set()
-    future_points = PriorityQueue()
-    future_points.put((0, (0, 0)))
-    while not future_points.empty():
-        (dist, point) = future_points.get()
-        past_points.add(point)
+    visited = set()
+    unvisited = PriorityQueue()
+    unvisited.put((0, (0, 0)))
+    while not unvisited.empty():
+        (dist, point) = unvisited.get()
+        visited.add(point)
         for neighbor in neighborhood(i_max, j_max, point[0], point[1]):
-            if part == "one":
-                next_dist = dist + matrix[neighbor[0]][neighbor[1]]
-            elif part == "two":
-                next_dist = dist + matrix_5x5(matrix, neighbor)
-            if neighbor not in past_points and delta_s[neighbor] > next_dist:
+            next_dist = dist + value(matrix, neighbor)
+            if neighbor not in visited and delta_s[neighbor] > next_dist:
                 delta_s[neighbor] = next_dist
-                future_points.put((next_dist, neighbor))
+                unvisited.put((next_dist, neighbor))
     return delta_s[i_max, j_max]
 
 
 def part_one():
-    ret = solution("one")
+    ret = solution(1)
     print("part one:", ret)
 
 
 def part_two():
-    ret = solution("two")
+    ret = solution(5)
     print("part two:", ret)
 
 
